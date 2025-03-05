7b425fe56d6d068859c063c05776635eeaacac41
JEFFHONTEZ <167432314+JEFFHONTEZ@users.noreply.github.com>
2025-03-05 17:52:16 +0300

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,75 @@
+import random
+import string
+
+PASSWORD_LETTERS = string.ascii_letters
+PASSWORD_DIGITS = string.digits
+PASSWORD_SPECIAL = string.punctuation
+
+def main():
+    print ("***************Password Generator**************")
+    while True:
+        password = generate_password()
+        print("Your generated password:", password)
+        print ("********************************************************************************")
+        response = input("Are you satisfied with this password? (yes/no): ").strip().lower()
+        if response == "yes":
+            print ("********************************************************************************")
+            print("Great! Your password has been generated above.")
+            break
+        elif response == "no":
+            print("Generating a new password...")
+            continue
+        else:
+            print("Invalid input. Please enter 'yes(y)' or 'no(n)'.")
+
+def generate_password():
+    min_password_length, max_password_length = define_password_length()
+    contains_special_characters = define_special_characters()
+    contains_digits = define_digits()
+    length = random.randint(min_password_length, max_password_length)
+
+    if contains_special_characters and contains_digits:
+        population = PASSWORD_LETTERS + PASSWORD_DIGITS + PASSWORD_SPECIAL
+    elif contains_special_characters:
+        population = PASSWORD_LETTERS + PASSWORD_SPECIAL
+    elif contains_digits:
+        population = PASSWORD_LETTERS + PASSWORD_DIGITS
+    else:
+        population = PASSWORD_LETTERS
+
+    return ''.join(random.choice(population) for _ in range(length))
+    
+def define_password_length():
+    min_password_length = int(input("Please eneter the minimum amount of characters for your password, as a number: "))
+    max_password_length = int(input("Please eneter the maximum amount of characters for your password, as a number: "))
+    print ("********************************************************************************")
+    if min_password_length > 50:
+        raise Exception ("Minimum character length cannot be greater than 50")
+    elif min_password_length <= 0:
+        raise Exception ("Minimum character length cannot be less than or equal to 0")
+    elif max_password_length > 50:
+        raise Exception ("Maximum character length cannot be greater than 100")
+    if max_password_length <= min_password_length:
+        raise Exception ("Maximum password length cannot be less than minimum")
+    else:
+        return (min_password_length, max_password_length)
+    
+def define_special_characters():
+    contains_special_characters = (input("Would you like your password to contain special characters, yes or no: ")).lower()
+    if contains_special_characters == "yes":
+        print ("********************************************************************************")
+        return True
+    else:
+        print ("********************************************************************************")
+        return False
+
+def define_digits():
+    contains_digits = (input("Would you like your password to contain digits, yes or no: ")).lower()
+    if contains_digits == "yes":
+        print ("**************************************************************")
+        return True
+    else:
+        print ("**************************************************************")
+        return False
+
+main ()
---
+++ strength_checker.py
@@ -0,0 +1,54 @@
+MAX_PASSWORD_LENGTH = 42
+MIN_PASSWORD_LENGTH = 12
+REQUIRED_PASSWORD_LENGTH = 12
+SPECIAL_CHARACTERS = "!@#$%^&*()-+?_=,<>/ "
+
+def main():
+    # password_path = Input()"passwords/password.txt"
+    password = input("please enter your password: ")
+    print ("------------------------Password Strength Checker------------------------")
+    print (" ")
+    password_strength_report(password)
+    print (" ")
+    print ("-------------------------------------------------------------------------")
+
+def get_password(password_path):
+    with open (password_path) as password:
+        return password.read()
+
+def check_length(password):
+    length_strength = 0
+    if len(password) > MAX_PASSWORD_LENGTH:
+        raise Exception ("Password cannot contain more than 42 characters")
+    if len(password) < MIN_PASSWORD_LENGTH:
+        raise Exception ("Password cannot contain less than 12 characters")
+    if len(password) >= REQUIRED_PASSWORD_LENGTH:
+        length_strength += 1
+    return (length_strength)
+        
+def check_password_strength(password):
+    uppercase_count = sum(1 for char in password if char.isupper())
+    digit_count = sum(1 for char in password if char.isdigit())
+    special_count = sum(1 for char in password if char in SPECIAL_CHARACTERS)
+    length_strength = 1 if check_length(password) else 0
+    return uppercase_count, digit_count, special_count, length_strength
+    
+def password_strength_report(password):
+    uppercase_count, digit_count, special_count, length_strength = check_password_strength(password)
+    password_strength = uppercase_count + digit_count + special_count + length_strength
+    missing_strengths = []
+    if length_strength == 0:
+        missing_strengths.append("length (should be at least 16 characters)")
+    if uppercase_count == 0:
+        missing_strengths.append("contain an uppercase character")
+    if digit_count == 0:
+        missing_strengths.append("a digit")
+    if special_count == 0:
+        missing_strengths.append("a special character")
+    
+    if missing_strengths:
+        print(f"Your password '{password}' is missing the following elements to be very strong: {', '.join(missing_strengths)}.")
+    else:
+        print(f"Congratulations! Your password '{password}' is very strong.")
+
+main()
